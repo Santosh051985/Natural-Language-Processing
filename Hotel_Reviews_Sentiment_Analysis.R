f3da4c147a743ea65421fc4e9cd6fabb07a8f8fc
Santosh Kumar <dssant@gmail.com>
2020-03-03 16:16:49 +0530

    Add files via upload

---
+++ Hotel_Reviews_Sentiment_Analysis.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(readr)
+library(lubridate)
+library(ggplot2)
+library(tidytext)
+library(tidyverse)
+library(stringr)
+library(tidyr)
+library(scales)
+library(broom)
+library(purrr)
+library(widyr)
+library(igraph)
+library(ggraph)
+library(SnowballC)
+library(wordcloud)
+library(reshape2)
+theme_set(theme_minimal())
+df <- read_csv("C:/Users/HP/Desktop/hilton-hawaiian-village/Hilton_Hawaiian_Village_Waikiki_Beach_Resort-Honolulu_Oahu_Hawaii__en.csv")
+df <- df[complete.cases(df), ]
+df$review_date <- as.Date(df$review_date, format = "%d-%b-%y")
+
+
+
+dim(df); min(df$review_date); max(df$review_date)
